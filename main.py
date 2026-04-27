205e6d996e4319491b6ea5798eb7268c2a67cfc2
Har0ld0 <gdmartinperez@gmail.com>
2026-04-27 16:00:34 +0200

    D: Added delta time and FPS limit to main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from logger import log_state
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -14,6 +17,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
     screen.fill("black")
     pygame.display.flip()
